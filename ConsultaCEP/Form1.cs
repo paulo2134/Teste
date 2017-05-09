6edc9927340bd44b07f6b7ffac15589a91457a7c
Paulo <paulocypriano@outlook.com>
2017-05-09 18:39:57 -0300

    Begging commit

---
+++ ConsultaCEP/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsultaCEP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.AtendeClienteClient consultar = new ServiceReference1.AtendeClienteClient("AtendeClientePort");
+
+            var resultado = consultar.consultaCEP(maskedTextBox1.Text);
+            if (resultado != null)
+            {
+                textBox2.Text = resultado.end;
+                textBox3.Text = resultado.cidade;
+                textBox4.Text = resultado.bairro;
+                textBox5.Text = resultado.complemento;
+                textBox6.Text = resultado.uf;
+
+
+            }
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+    }
+}
